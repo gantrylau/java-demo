968db857fe356a7115bbb3e743016999ebcdd759
gantrylau <gantrylau@qq.com>
2017-01-20 22:55:07 +0800

    eventbus demo

---
+++ eventbus-test/src/main/java/com/gl/test/eventbus/ServerMain.java
@@ -0,0 +1,8 @@
+package com.gl.test.eventbus;
+
+/**
+ * @author gantrylau
+ * @since 2017/1/20
+ */
+public class ServerMain {
+}
---
+++ eventbus-test/src/main/java/com/gl/test/eventbus/handler/EventBusManager.java
@@ -0,0 +1,8 @@
+package com.gl.test.eventbus.handler;
+
+/**
+ * @author gantrylau
+ * @since 2017/1/20
+ */
+public class EventBusManager {
+}
